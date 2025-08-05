e0d926e6b5be7572bf84e89761b3f591ffed5020
achhangani <aditi.chhangani@siriusxm.com>
2025-08-05 19:09:36 +0000

    new change!

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ h1, h3 {
 # --- Title Section (smaller + tighter) ---
 st.markdown("""
 <div style='text-align:center; padding-top:-20px;margin-top:-20px;'>
-    <h1 style='font-size:26px;font-family: Georgia;'>SXM Satellite Directory</h1>
+    <h1 style='font-size:26px;font-family: Georgia;'>SXM Satellite Engineering</h1>
     <h3 style='font-size:16px;font-family: Georgia;'>Choose your satellite below</h3>
 </div>
 """, unsafe_allow_html=True)
